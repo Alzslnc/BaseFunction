4a9a462159a85b5839b2c39804c57779906e32c3
DESKTOP1\Alz <alzslnc@gmail.com>
2024-02-06 06:59:05 +0300

    test

--- Test.cs
+++ Test.cs
@@ -18,8 +18,61 @@ namespace BaseFunction
 {
     public class Test
     {
-        [CommandMethod("test01")]
+        [CommandMethod("Test01")]
         public void Test01()
+        {
+            if (!TryGetobjectId(out ObjectId contourid, new List<Type>
+            { typeof(Arc), typeof(Polyline), typeof(Line), typeof(Circle) }, "Выберите контур в плане")) return;
+            if (!TryGetPointFromUser(out Point3d point, true, "Выберите точку на фасаде соответствующую началу контура на нулевой отметке")) return;
+            if (!TryGetObjectsIds(out List<ObjectId> ids, typeof(BlockReference), "Выберите закладные")) return;
+
+            using (Transaction tr = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction())
+            {
+                using (Curve c = tr.GetObject(contourid, OpenMode.ForRead, false, true) as Curve)
+                {
+                    double length = c.GetLength();
+
+                    double x = point.X;
+                    double y = point.Y;
+
+                    if (c != null)
+                    {
+                        List<Entity> entities = new List<Entity>();
+
+                        foreach (ObjectId id in ids)
+                        {
+                            using (BlockReference br = tr.GetObject(id, OpenMode.ForRead) as BlockReference)
+                            {
+                                if (br == null) continue;
+
+                                double offset = br.Position.X - x;
+                                if (offset <= 0 || offset > length) continue;
+
+                                double height = br.Position.Y - y;
+
+                                Point3d position = c.GetPointAtDist(offset);
+
+                                entities.Add(new DBText
+                                {
+                                    Position = position,
+                                    TextString = height.ToString("F2"),
+                                    Justify = AttachmentPoint.BaseLeft
+                                });
+
+                                entities.Add(new DBPoint(position));
+                            }
+                        }
+
+                        entities.AddEntityInCurrentBTR();
+                    }
+                }
+                tr.Commit();
+            }
+
+        }
+
+        [CommandMethod("test02")]
+        public void Test02()
         {
             if (!TryGetObjectsIds(out List<ObjectId> ids, new List<Type> { typeof(MText), typeof(DBText) })) return;
             using (Transaction tr = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction())
